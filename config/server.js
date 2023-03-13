2493eb650d21434f2780550837e94aec5a2354b5
felipemimoura <felipemimoura@gmail.com>
2023-03-13 11:40:16 -0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '77993559c64fa4c177c2886dde920d5c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '456a5c41-d2f6-40c2-92c7-b364c5139789'
+};
